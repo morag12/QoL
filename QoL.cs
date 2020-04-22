8d8df0c9b5dc0c689c6af584215de980b30c09b4
Yaekith <47884923+Yaekith@users.noreply.github.com>
2020-04-22 09:46:49 +0800

    a

---
+++ QoL.cs
@@ -0,0 +1,100 @@
+﻿using Harmony;
+using MelonLoader;
+using NET_SDK;
+using NET_SDK.Harmony;
+using Notorious;
+using Notorious.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRC;
+using Random = System.Random;
+
+namespace QoL
+{
+    public class QoL : MelonMod
+    {
+        public static bool Flight = false;
+
+        public static List<GameObject> gameObjects = new List<GameObject>();
+
+        public static Vector3 Grav = Physics.gravity;
+
+        public override void OnApplicationStart()
+        {
+            try
+            { 
+                var HarmonyInstance = Manager.CreateInstance("Quality Assurance");
+                var API = SDK.GetClass("VRC.Core", "API");
+                var Amp = NET_SDK.SDK.GetClass("AmplitudeSDKWrapper", "AmplitudeWrapper");
+                var moderationManager = SDK.GetClass("ModerationManager");
+                HarmonyInstance.Patch(API.GetMethod("DeviceID"), AccessTools.Method(typeof(QoL), "HWIDSpoofer"));
+                HarmonyInstance.Patch(Amp.GetMethod("InitializeDeviceId"), AccessTools.Method(typeof(QoL), "HWIDSpoofer"));
+                HarmonyInstance.Patch(API.GetMethod("IsOffline"), AccessTools.Method(typeof(QoL), "TruePrefix"));
+            }
+            catch(Exception e)
+            {
+                MelonModLogger.LogError("An exception has occurred. Dm Yaekith#1337 on discord. This mod may be outdated.");
+                MelonModLogger.LogError(e.ToString());
+            }
+            finally
+            {
+                MelonModLogger.Log("Quality of Life has been applied. You can now fucking play this game without being forcekicked, with a hwid spoofer and you can also see people who have blocked you!");
+                MelonModLogger.Log("Your New HWID: " + VRC.Core.API.DeviceID);
+                MelonModLogger.Log("IsOffline: " + VRC.Core.API.IsOffline());
+            }
+          
+        }
+        public override void OnUpdate()
+        {
+            if (gameObjects.Count() == 0)
+            {
+                Transform parent = Wrappers.GetQuickMenu().transform.Find("ShortcutMenu");
+
+                if (parent != null)
+                {
+                    var button = ButtonAPI.CreateButton(ButtonType.Toggle, "Flight", "Enable/Disable Flight (now in vr kek)", Color.white, Color.cyan, 1, 1, parent, new Action(() =>
+                    {
+                        Flight = true;
+                        Physics.gravity = Flight ? Vector3.zero : Grav;
+                        MelonModLogger.Log($"Flight has been {(Flight ? "Enabled." : "Disabled")}");
+                    }), new Action(() =>
+                    {
+                        Flight = false;
+                        Physics.gravity = Flight ? Vector3.zero : Grav;
+                        MelonModLogger.Log($"Flight has been {(Flight ? "Enabled." : "Disabled")}");
+                    }));
+
+                    gameObjects.Add(button.gameObject);
+                }
+            }
+        }
+        public static bool TruePrefix(ref bool __result)
+        {
+            __result = true;
+            return true;
+        }
+        private static bool AntiOverrides(ref bool __result, string __0, string __1, string __2)
+        {
+            __result = false;
+            return false;
+        }
+
+        public static string HWIDSpoofer()
+        {
+            var crypt = new SHA256Managed();
+            string hash = string.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(new Random().Next(1, 999999999).ToString()));
+            foreach (byte theByte in crypto)
+            {
+                hash += theByte.ToString("x2");
+            }
+            return hash;
+        }
+    }
+}
---
+++ Wrappers.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using VRC;
+using VRC.Core;
+
+namespace Notorious
+{
+    public static class PlayerWrappers
+    {
+        public static VRCPlayer GetCurrentPlayer(this PlayerManager instance)
+        {
+            return instance.field_List_1_Player_0.get_Item(0).field_VRCPlayer_0;
+        }
+        public static Il2CppSystem.Collections.Generic.List<Player> GetAllPlayers(this PlayerManager instance)
+        {
+            return instance.field_List_1_Player_0;
+        }
+        public static APIUser GetAPIUser(this Player player)
+        {
+            return player.field_APIUser_0;
+        }
+        public static Player GetVRC_Player(this VRCPlayer player)
+        {
+            return player.field_Player_0;
+        }
+        public static Player GetPlayer(this PlayerManager instance, string UserID)
+        {
+            var Players = instance.GetAllPlayers();
+            Player FoundPlayer = null;
+            for(int i = 0; i < Players.Count; i++)
+            {
+                var player = Players.get_Item(i);
+                if (player.GetAPIUser().id == UserID)
+                {
+                    FoundPlayer = player;
+                }
+            }
+
+            return FoundPlayer;
+        }
+        public static Player GetPlayer(this PlayerManager instance, int Index)
+        {
+            var Players = instance.GetAllPlayers(); 
+            return Players.get_Item(Index);
+        }
+        public static Player GetSelectedPlayer(this QuickMenu instance)
+        {
+            var APIUser = instance.field_APIUser_0;
+            var playerManager = Wrappers.GetPlayerManager();
+            return playerManager.GetPlayer(APIUser.id);
+        }
+    }
+    public static class Wrappers
+    {
+        public static PlayerManager GetPlayerManager()
+        {
+            return PlayerManager.Method_Public_21();
+        }
+        public static QuickMenu GetQuickMenu()
+        {
+            return QuickMenu.prop_QuickMenu_0;
+        }
+        public static VRCUiManager GetVRCUiPageManager()
+        {
+            return VRCUiManager.field_VRCUiManager_0;
+        }
+        public static UserInteractMenu GetUserInteractMenu()
+        {
+            return Resources.FindObjectsOfTypeAll<UserInteractMenu>()[0];
+        }
+        public static GameObject GetPlayerCamera()
+        {
+            return GameObject.Find("Camera (eye)");
+        }
+
+        public static string GetRoomId()
+        {
+            return APIUser.CurrentUser.location;
+        }
+
+        public static void SetToolTipBasedOnToggle(this UiTooltip tooltip)
+        {
+            UiToggleButton componentInChildren = tooltip.gameObject.GetComponentInChildren<UiToggleButton>();
+
+            if (componentInChildren != null && !string.IsNullOrEmpty(tooltip.alternateText))
+            {
+                string displayText = (!componentInChildren.toggledOn) ? tooltip.alternateText : tooltip.text;
+                if (TooltipManager.field_Text_0 != null) //Only return type field of text
+                {
+                    TooltipManager.Method_Public_String_3(displayText); //Last function to take string parameter
+                }
+                else if (tooltip.tooltip != null)
+                {
+                     tooltip.tooltip.text = displayText;
+                }
+            }
+        }
+        public static void SetPosition(this Transform transform, float x_pos, float y_pos)
+        {
+            //localPosition
+            var quickMenu = Wrappers.GetQuickMenu();
+
+            float X = quickMenu.transform.Find("UserInteractMenu/ForceLogoutButton").localPosition.x - quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
+            float Y = quickMenu.transform.Find("UserInteractMenu/ForceLogoutButton").localPosition.x - quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
+
+            transform.transform.localPosition = new Vector3(X * x_pos, Y * y_pos);
+        }
+    }
+}
